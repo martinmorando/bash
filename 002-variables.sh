bf90cee9d51482dc13b399773cabb20f17145108
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 10:55:18 -0300

    add 002-variables.sh

---
+++ 002-variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# Variables
+#
+
+# Declare and assign variables (no whitespace)
+name="Ernst Stavro Blofeld"
+
+# Reassign
+name="James Bond"
+
+# Constant
+readonly code=007
+# code=009            # This can't happen 
+ 
+# Print
+echo "His name is $name and the code is $code."
