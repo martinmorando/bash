f2399dfe70e9155cbdce66528dda26ec91ec358b
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 10:54:46 -0300

    rename 001-basics.sh to 001-hello-satoshi.sh, and explain 'shebang' and output

---
+++ 001-hello-satoshi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The first line is known as "shebang" and indicates
+# the script needs to be run using the Bash shell.
+
+#
+#   Hello Satoshi
+#
+
+# This is a single-line comment 
+
+# No multi-line comments but
+# you can do this.
+
+# Output to console
+echo "Hello, Satoshi!"
