f614adc14eeb227dbe34ea7c2b7f558a32f6ce0c
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-24 15:25:31 -0300

    add 003-logic/

---
+++ 003-logic/003-logic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Logic
+#
+
+she_says="The teacher says you won't be able do it"
+
+# Conditional
+if [ "$she_says" = "The teacher says you won't be able do it" ]; then
+    i_reply="The teacher can f*ck off"
+else
+    i_reply=""
+fi
+
+echo $i_reply
+
+# Notes:
+# - The spaces around the brackets ([ ]), the semicolon (;) and 
+#   the double quotes ("") are required.
+# - End if with "fi".
