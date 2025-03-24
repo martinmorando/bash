4f86fc4997b231b69eef1240c31febc716ea4bdc
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-24 15:26:53 -0300

    add 004-list/

---
+++ 004-list/004-list.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+# List
+# -> ls [options]
+#
+
+
+# - List files and folders in the current directory
+ls
+
+
+
+# [OPTIONS]: combinable
+# -a (all): includes hidden files (those starting with a dot)
+ls -a
+
+# -l (long format): includes permissions, modification date and other details
+ls -l
+
+# -t (time): sort by last modification time
+ls -t
+
+# -S (size): sort by size
+ls -S
+
+
+
+# [TRICKS]
+# - List all files in the current directory with the extension .txt
+ls *.txt
