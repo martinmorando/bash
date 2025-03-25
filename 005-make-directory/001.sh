13727d6bba018aa5f559610542d79d9791bae2d5
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-25 16:25:10 -0300

    add 005-make-directory/

---
+++ 005-make-directory/001.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Create directories
+# mkdir [options] [directoryName]
+#
+
+# Create one directory
+mkdir conquest_plans
+
+
+# Create several directories
+mkdir code_lessons top_secret_docs_found_on_the_internet 
+
+
+# Try to create a directory that already exists
+mkdir code_lessons      # Output: mkdir: cannot create directory 'code_lessons': File exists
+
+
+# [OPTIONS]
+# -v (verbose)
+mkdir -v is_this_legal # Ourput: mkdir: created directory 'is_this_legal'
+
+# -m (permissions)
+mkdir -m 755 stolen_memes
