52e069213ce348d80c9acab08a3c0dbae6024f41
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-26 18:01:40 -0300

    add 006-change-directory/

---
+++ 006-change-directory/001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Change directory
+# cd [pathToDirectory]
+#
+
+# Move to directory "here" (inside the current directory)
+cd here/
+
+
+# Move to parent directory (of the current directory)
+cd ..
+
+
+# Move to previous directory
+cd -
