5744c0d628d86f6a021b58ea5055b2e8817b52ce
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-26 18:02:42 -0300

    add 007-touch/

---
+++ 007-touch/001.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Touch
+# touch [fileName.ext]
+#
+
+
+# If the file does not exist, create it (empty)
+touch new.txt
+
+
+# If the file exists, does not overwrite it but 
+# updates its last modified timestamp.
+touch new.txt
