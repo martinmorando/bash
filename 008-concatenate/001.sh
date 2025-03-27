f94e4c16fa5f71f58afa11bb2040fac4d9ac27de
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-27 20:38:22 -0300

    add 008-concatenate/

---
+++ 008-concatenate/001.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Concatenate
+# cat [options] [fileName1] [fileName2]
+#
+
+# Display content of filename.txt
+cat filename.txt
+
+# Display content of several files
+cat filename1.txt filename2.csv
+
+# Display content of filename.txt with line numbers
+cat -n filename.txt
