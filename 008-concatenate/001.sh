f21b81e23619912f9e52419aa121a958456bb067
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-28 20:47:49 -0300

    add example on creating a file in 008-concatenate/001.sh

--- 008-concatenate/001.sh
+++ 008-concatenate/001.sh
@@ -13,3 +13,15 @@ cat filename1.txt filename2.csv
 
 # Display content of filename.txt with line numbers
 cat -n filename.txt
+
+# Create an empty file, and the console prompts for user input. Write
+# and save with Ctrl + d. Overwrites file.
+cat > notes.txt
+
+# Notes:
+# - The file extension is a convention, it's not necessary.
+#  
+# - It's also possible to use more file extensions (.sh, .yaml, .html, etc).
+#
+# - Creating a file with .png extension won't create a valid image file 
+#   because it wouldn't have the binary data that conforms a PNG image file format.
