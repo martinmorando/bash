81e3a3c74d602c781ff114ab134f8b3c1e97f8d0
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-06-02 13:49:23 -0300

    add text-editor-nano/

---
+++ text-editor-nano/001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 
+# Nano
+# - Command: nano [file]
+# - Terminal-based text editor.
+# - If the text file exists, it opens it. If not, it can create a new one.
+# 
+ 
+nano file.txt
+
+# Keyboard shortcuts: (^ is Ctrl)
+# ^O: save
+# ^G: help
+# ^X: exit
+# ^K: cut
+# ^U: paste
